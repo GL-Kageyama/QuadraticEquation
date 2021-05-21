9c82cc633b39430cc98885b98bbda0d9c8ca8f3f
影山榮一 <a1kageyama@kageyamBookpuro.AirPort>
2021-05-21 17:15:23 +0900

    Update

--- QuadraticEquation.cs
+++ QuadraticEquation.cs
@@ -6,61 +6,64 @@ namespace Quadratic
     {
         public static void Main(string[] args)
         {
-            QuadraticFunc func = new QuadraticFunc();
+            QuadraticFunc quadraticFunc = new QuadraticFunc();
 
-            //問1を求める
-            func.Quadratic_No1();
+            // Asking for Question 1
+            quadraticFunc.QuadraticNumber1();
 
-            //問2を求める
-            func.Quadratic_No2();
+            // Asking for Question 2
+            quadraticFunc.QuadraticNumber2();
 
-            //問3を求める
-            func.Quadratic_No3();
+            // Asking for Question 3
+            quadraticFunc.QuadraticNumber3();
         }
     }
 
     class QuadraticFunc
     {
-        //2次方程式の解の公式(プラスVer.)
+        // Formulas for solutions of quadratic equations (Plus Ver.)
         public double QuadraticArgPlus(int a, int b, int c)
         {
             double x1 = (-b + Math.Sqrt(b*b - 4*a*c)) / 2*a;
             return x1;
         }
 
-        //2次方程式の解の公式(マイナスVer.)
+        // Formulas for solutions of quadratic equations (minus Ver.)
         public double QuadraticArgMinus(int a, int b, int c)
         {
             double x2 = (-b - Math.Sqrt(b*b - 4*a*c)) / 2*a;
             return x2;
         }
 
-        //解の出力
+        // Outputting the solution
         public void Ans(double x1, double x2)
         {
             if (double.IsNaN(x1) == false)
             {
                 if (double.IsNaN(x2) == false)
                 {
-                    Console.Write("xの解は以下の2つです。\r\n");
+                    Console.Write("There are two solutions for x.\r\n");
                     Console.Write("x1 = {0}\r\n", x1);
                     Console.Write("x2 = {0}\r\n\r\n", x2);
                 }
             }
             else
             {
-                Console.Write("解が虚数になるため求められません。\r\n\r\n");
+                Console.Write("It cannot be obtained because the solution is imaginary.\r\n\r\n");
             }
         }
 
-        //問1の解法
-        public void Quadratic_No1()
+        // Solution to Question 1
+        public void QuadraticNumber1()
         {
-            Console.Write("2次方程式 a*x*x + b*x + c = 0 における解を求めます。\r\n");
-            Console.Write("a, b, cの値を順に入力して下さい。\r\n");
+            Console.Write("Find the solution to the quadratic equation a*x*x + b*x + c = 0\r\n");
+            Console.Write("Enter the values for a, b, and c in order.\r\n");
 
+            Console.Write("a = ");
             int a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
             int b = int.Parse(Console.ReadLine());
+            Console.Write("c = ");
             int c = int.Parse(Console.ReadLine());
 
             double x1 = this.QuadraticArgPlus(a, b, c);
@@ -69,14 +72,17 @@ namespace Quadratic
             this.Ans(x1, x2);
         }
 
-        //問2の解法
-        public void Quadratic_No2()
+        // Solution to Question 2
+        public void QuadraticNumber2()
         {
-            Console.Write("2次方程式 a*(x - p)*(x - p) + q = 0 における解を求めます。\r\n");
-            Console.Write("a, p, qの値を順に入力して下さい。\r\n");
+            Console.Write("Find the solution to the quadratic equation a*(x-p)*(x-p) + q = 0\r\n");
+            Console.Write("Enter the values for a, p, and q in order.\r\n");
 
+            Console.Write("a = ");
             int a = int.Parse(Console.ReadLine());
+            Console.Write("p = ");
             int p = int.Parse(Console.ReadLine());
+            Console.Write("q = ");
             int q = int.Parse(Console.ReadLine());
 
             double x1 = this.QuadraticArgPlus(a, -2*a*p, a*p*p + q);
@@ -85,15 +91,19 @@ namespace Quadratic
             this.Ans(x1, x2);
         }
 
-        //問3の解法
-        public void Quadratic_No3()
+        // Solution to Question 3
+        public void QuadraticNumber3()
         {
-            Console.Write("2次方程式 (a*x + b)*(c*x + d)= 0 における解を求めます。\r\n");
-            Console.Write("a, b, c, dの値を順に入力して下さい。\r\n");
+            Console.Write("Find the solution to the quadratic equation (a*x + b)*(c*x + d) = 0\r\n");
+            Console.Write("Enter the values for a, b, c, and d in order.\r\n");
 
+            Console.Write("a = ");
             int a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
             int b = int.Parse(Console.ReadLine());
+            Console.Write("c = ");
             int c = int.Parse(Console.ReadLine());
+            Console.Write("d = ");
             int d = int.Parse(Console.ReadLine());
 
             double x1 = this.QuadraticArgPlus(a*c, a*d + b*c, b*d);
